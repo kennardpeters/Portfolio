37438e0f3096fdadaa58aa11f6be7c04555c6922
Kennard Peters <kenpo1999@gmail.com>
2021-02-15 14:42:50 -0800

    updated bio styling

--- src/components/about.js
+++ src/components/about.js
@@ -38,7 +38,7 @@ export default function About() {
                             Hello, I'm{" "}
                             <span className="text-green-100">{author.name}</span>
                         </h1>
-                        <div className="prose lg:prose-xl text-blue-400">
+                        <div className="prose lg:prose-xl text-white-800">
                             <BlockContent blocks={author.bio} projectId="w6gbeasc" dataset="production" />
                         </div>
                     </div>
