0d148f4d4d66f78849db399b1fd34d8c5fd40a73
Kennard Peters <kenpo1999@gmail.com>
2021-02-15 15:39:46 -0800

    Improved text font

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -11,7 +11,7 @@ export default function NavBar() {
                     to="/" 
                     exact 
                     activeClassName="text-white"
-                    className="inflex-flex items-center py-6 px-3 mr-4 text-green-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-green-100 hover:text-green-800 text-4xl font-bold crimson tracking-widest"
                     >
                         Kennard
                     </NavLink>
