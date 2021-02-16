4639a972642a20f4d31b9fc2668ed6f18c179689
Kennard Peters <kenpo1999@gmail.com>
2021-02-16 08:34:27 -0800

    Cohesive styling

--- src/components/about.js
+++ src/components/about.js
@@ -29,14 +29,14 @@ export default function About() {
         <main className="relative">
             <img src={ivshot} alt="IV Scene" className="absolute w-full" />
             <div className="p-10 lg:pt-48 container mx-auto relative">
-                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+                <section className="bg-black rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} 
                     className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
                     alt={author.name} />
                     <div className="text-lg flex-col justify-center">
-                        <h1 className="crimson text-6xl text-green-300 mb-4">
+                        <h1 className="crimson text-6xl text-yellow-300 mb-4">
                             Hello, I'm{" "}
-                            <span className="text-green-100">{author.name}</span>
+                            <span className="text-yellow-100">{author.name}</span>
                         </h1>
                         <div className="crimson lg:crimson-xl text-white">
                             <BlockContent blocks={author.bio} projectId="w6gbeasc" dataset="production" />
--- src/components/post.js
+++ src/components/post.js
@@ -24,9 +24,9 @@ export default function Post() {
     }, []);
     
     return (
-        <main className="bg-green-100 min-h-screen p-12">
+        <main className="bg-black min-h-screen p-12">
             <section className="container mx-auto">
-                <h1 className="text-5xl flex justify-center cursvie">Blog Posts Page</h1>
+                <h1 className="text-5xl flex justify-center crimson text-yellow-100">Blog Posts Page</h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {postData && postData.map((post, index) => (
